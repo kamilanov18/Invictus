16aa25601c71e7c842ea6b83d564fd7033421719
kamilanov18 <54852657+kamilanov18@users.noreply.github.com>
2021-07-06 21:33:37 +0300

    Add Validations class and more changes
    * Add Validations class with several methods
    * Add colors to the Log class when there is system information or an error
    * Add colors to package.json

--- app/src/DB.js
+++ app/src/DB.js
@@ -1,26 +1,61 @@
 const sql = require("mssql/msnodesqlv8");
-const config = require(".\\config");
+const { config } = require("./config");
+const colors = require("colors");
+
+class Validations {
+    static #pattern
+
+    static validateName(name) {
+
+        if(!name)
+            throw new Error("Names can't be empty!");
+        
+        this.#pattern=/^[a-z]/;
+        if(this.#pattern.test(name))
+            throw new Error("Names have to start with a capital letter!");
+
+        this.#pattern=/[0-9]/;
+        if(this.#pattern.test(name))
+            throw new Error("Names can't contain numbers!");
+
+        this.#pattern=/[!@$%^&*()_+=|\/\\\[\]{}#?<>;:~]/;
+        if(this.#pattern.test(name))
+            throw new Error("Names can't contain special symbols other than: ' \" ` , ");
+    }
+
+    static validateTitle(title) {
+        if(!title)
+            throw new Error("Titles can't be empty!");
+
+        if(title.length <= 50 && title.length<=0)
+            throw new Error("Titles have to be less than 50 characters!");
+    }
+
+    static validateDescription(desc) {
+        if(!desc)
+            throw new Error("Descriptions can't be empty!");
+
+        if(desc.length <= 250 && desc.length<=0)
+            throw new Error("Titles have to be less than 250 characters");
+    }
+}
 
 class Log {
-    getDate() {
+
+    static #getDate() {
         let date_ob = new Date();
-        return `(${date_ob.getDate()}.${date_ob.getMonth()+1}.${date_ob.getYear()+1900} ${date_ob.getHours()}:${date_ob.getMinutes()}:${date_ob.getSeconds()}) DB.js: `;
+        return `(${date_ob.getDate()}.${date_ob.getMonth()+1}.${date_ob.getYear()+1900} ${date_ob.getHours()}:${date_ob.getMinutes()}:${date_ob.getSeconds()})`;
     }
 
-    getFuncName(funcNum) {
-        
+    static logError(funcName,err) {
+        console.log(`${"[!]".red.bold} ${this.#getDate().yellow} DB.js: function ${funcName} encountered an error: ${err}`);
     }
 
-    logError(funcNum,err) {
-        console.log(`[!] ${this.getDate()} DB.js: function ${this.getFuncName(funcNum)} encountered an error: ${err}`);
+    static logInfo(funcName) {
+        console.log(`${"[i]".blue.bold} ${this.#getDate()} DB.js: function ${funcName} successfully executed`);
     }
 
-    logInfo(funcNum) {
-        console.log(`[i] ${this.getDate()} DB.js: function ${this.getFuncName(funcNum)} successfully executed`);
-    }
 };
-
-const logger = new Log();
 
 class DBManager {
     #pool
@@ -30,14 +65,13 @@ class DBManager {
 
     async createUser() {
         try {
-            let pool = await this.#pool;
+            const pool = await this.#pool;
         } catch(err) {
-            console.log(err);
-            return 420;
+            Log.logError("createUser",err);
         }
     }
 };
 
 const DBM = new DBManager();
 
-module.exports = { DBM,logger };
+module.exports = { DBM,Log, Validations };

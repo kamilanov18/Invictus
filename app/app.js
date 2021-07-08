7c9ee4091ac2ce7a893d036ffc95e194c3d0a484
kamilanov18 <54852657+kamilanov18@users.noreply.github.com>
2021-07-08 21:23:58 +0300

    Layout basic express infrastructure
    * Start development on app.js
    * Install express related packages
    * Add admin.ejs, main.ejs and user.ejs

--- app/app.js
+++ app/app.js
@@ -1,14 +1,66 @@
 const express = require("express");
+const session = require("express-session")
+const bodyParser = require("body-parser")
+const { sessionConfig } = require("./src/config");
+const { DBM } = require("./src/DB");
 const app = express();
 
-app.get("/test", (req,res)=>{
-    res.send("aaaa");
-});
+app.use(session({
+    name: sessionConfig.name,
+    resave: false,
+    saveUninitialized: false,
+    secret: sessionConfig.secret,
+    cookie: {
+        maxAge: sessionConfig.maxAge,
+        sameSite: true,
+        secure: false
+    }
+}));
 
-app.get("*", (req,res)=>{
-    res.send("Error, no page like this exists yet.");
-});
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
 
 app.listen(3000, ()=>{
     console.log("Server started");
+});
+
+// Views //
+
+app.get("/", (req, res)=>{
+    res.render("../views/main.ejs");
+})
+
+app.get("/admin", (req, res)=>{
+    res.render("../views/admin.ejs");
+})
+
+app.get("/user", (req, res)=>{
+    res.render("../views/user.ejs");
+})
+
+app.get("*", (req,res)=>{
+    res.render("../views/main.ejs");
+});
+
+// Main api functionality //
+app.post("/login", async (req,res, next)=>{
+    const {
+        username,
+        password
+    } = req.body;
+
+    const userId = await DBM.validateUser(username,password);
+
+    if(userId) {
+        req.session.userId = userId;
+        const { isAdmin } = await DBM.getPublicUserDataById(userId);
+        if(isAdmin) {
+            res.redirect("/admin");
+        } else {
+            res.redirect("/user");
+        } 
+    } else {
+        res.redirect("/");
+    }
 });
